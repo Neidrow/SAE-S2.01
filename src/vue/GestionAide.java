1d12adbb9eab81024161b4975d18c85dde9f27d2
Adrien Vigué <adrien.vigue@A304B2.iut.local>
2024-05-31 14:31:49 +0200

    ajout javadoc

--- src/vue/GestionAide.java
+++ src/vue/GestionAide.java
@@ -1,15 +1,37 @@
+/*
+ * GestionnaireAide.java				31/05/24
+ * IUT de Rodez, pas de copyright 
+ */
+
 package vue;
 
+/**
+ * TODO définir à quoi sert la classe
+ */
 public class GestionAide {
+	
+	/**
+	 * Affiche les coups possibles par un pion sélectionné lors de
+	 * ce tour
+	 */
     public void aideMouvement(Piece pieceSelectionnee) {
-        // Afficher les coups possibles par un pion sélectionné
+        // TODO Afficher les coups possibles par un pion sélectionné
     }
 
+    /**
+     * Lorsqu'un joueur à gagné, fait apparaitre une fenêtre avec le
+     * nom de l'équipe gagnante
+     */
     public void victoire() {
-        // Afficher quel joueur a gagné lorsque les conditions de victoire sont remplies
+        // TODO Afficher quel joueur a gagné lorsque les conditions de victoire sont remplies
     }
 
+    /**
+     * si le joueur essai de faire un coup qui n'est pas autorisé par
+     * le jeu, afficher une fenêtre signalant que le coup demandé est
+     * impossible
+     */
     public void interdiction() {
-        // Afficher quand un joueur fait un coup impossible
+        // TODO Afficher quand un joueur fait un coup impossible
     }
 }
