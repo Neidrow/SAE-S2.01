511f6174c1f5e28082b0de0cc2419c811ee646a4
Xavier Taborda Rodrigo <rodrigo.xaviertaborda@iut-rodez.fr>
2024-06-05 14:55:18 +0200

    avancé des liaisons des scènes

--- src/vue/Main.java
+++ src/vue/Main.java
@@ -1,20 +1,59 @@
 package vue;
 
 import javafx.application.Application;
+
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+	
+	private static Scene AffichagePrincipal;
+	private static Scene AffichagePrincipalMenu;
+	private static Scene Parametres;
+	private static Stage fenetrePrincipale;
+	
+	public static void activerAffichagePrincipalMenu() {
+        fenetrePrincipale.setScene(AffichagePrincipalMenu);
+    }
+	
+	public static void activerParametres() {
+        fenetrePrincipale.setScene(Parametres);
+    }
+
     @Override
     public void start(Stage primaryStage) {
-            Plateau ui = new Plateau();
-            BorderPane root = ui.creerContenu(primaryStage);
-            Scene scene = new Scene(root, 1200, 1000);
-            primaryStage.setScene(scene);
-            primaryStage.setTitle("Menu Principal");
-            primaryStage.show();
+           
+            
+            try { 
+            	FXMLLoader chargeurFXMLAffichagePrincipal = new FXMLLoader();
+                chargeurFXMLAffichagePrincipal.setLocation(getClass().getResource("AffichagePrincipal.fxml"));
+                Parent conteneur = chargeurFXMLAffichagePrincipal.load();
+                AffichagePrincipal = new Scene(conteneur);
+                
+                FXMLLoader chargeurFXMLAffichagePrincipalMenu = new FXMLLoader();
+                chargeurFXMLAffichagePrincipalMenu.setLocation(getClass().getResource("AffichagePrincipalMenu.fxml"));
+                conteneur = chargeurFXMLAffichagePrincipalMenu.load();
+                AffichagePrincipalMenu = new Scene(conteneur);
+                
+                FXMLLoader chargeurFXMLParametres = new FXMLLoader();
+                chargeurFXMLParametres.setLocation(getClass().getResource("Parametres.fxml"));
+                conteneur = chargeurFXMLParametres.load();
+                Parametres = new Scene(conteneur);
+             // on définit le titre, la hauteur et la largeur de la fenêtre principale
+                primaryStage.setTitle("Jeu de dames");
+                
+                primaryStage.setScene(AffichagePrincipal);
+                fenetrePrincipale = primaryStage;
+                primaryStage.show();
+                
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
     }
 
     public static void main(String[] args) {
